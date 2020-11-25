8edce9cc5c6c9107f7c3502c75cdacbcc035d601
Marcellus Parley <mseanparley@gmail.com>
2020-11-25 11:56:43 -0800

    Part 8b

---
+++ part08/library-frontend/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  return (
+    <div>
+      <Notification errorMessage={errorMessage} />
+
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+        setError={notify}
+      />
+
+    </div>
+  )
+}
+
+const Notification = ({errorMessage}) => {
+  if (!errorMessage) {
+    return null
+  }
+
+  return (
+    <div style={{color: 'red'}}>
+      {errorMessage}
+    </div>
+  )
+}
+
+export default App
---
+++ part08/library-frontend/src/components/Authors.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+
+const Authors = (props) => {
+  const resultAuthors = useQuery(ALL_AUTHORS)
+  const [authorName, setAuthorName] = useState('')
+  const [authorBorn, setAuthorBorn] = useState('')
+  //const [authors, setAuthors] = useState([])
+  const [changeAuthor] = useMutation(EDIT_AUTHOR, {
+    //refetchQueries: [ {query: ALL_AUTHORS }],
+    onError: (error) => {
+      props.setError(error.toString())
+    }
+  })
+
+  if (!props.show) {
+    return null
+  }
+
+  if (resultAuthors.loading)
+    return <div>Loading...</div>
+
+  const authors = resultAuthors.data.allAuthors
+
+  const submit = (event) => {
+    event.preventDefault()
+
+    changeAuthor({ variables: {
+      name: authorName, setBornTo: parseInt(authorBorn)
+    }})
+
+    setAuthorBorn('')
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+      <h3>Set Birthyear</h3>
+      <form onSubmit={submit}>
+        <select value={authorName} 
+          onChange={({ target }) => {setAuthorName(target.value)}}>
+
+          <option disabled selected value=''>Select Author</option>
+
+          {authors.map(a => 
+            <option key={a.id} value={a.name}>{a.name}</option>)}
+
+        </select>
+        <label> Born
+          <input type='number' value={authorBorn} 
+            onChange={({ target }) => {setAuthorBorn(target.value)}} />
+        </label>
+
+        <button type='submit'>Update</button>
+      </form>
+    </div>
+  )
+}
+
+export default Authors
